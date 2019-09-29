7b6e9fe68e38c969fb084f0904e9411457281740
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-29 13:15:30 +0200

    generating the getter and setter for the news class

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -8,4 +8,41 @@ public class News {
     public String news;
     public int departId;
 
+    public News(String topic, String news, int departId) {
+        this.topic = topic;
+        this.news = news;
+        this.departId = departId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public String getNews() {
+        return news;
+    }
+
+    public int getDepartId() {
+        return departId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    public void setDepartId(int departId) {
+        this.departId = departId;
+    }
 }
