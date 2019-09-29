7742a6c49f2533f6b936797ba035b08f085f07d5
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-09-29 13:17:41 +0200

    generating the getter and setter for the users class

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -5,15 +5,53 @@ public class Users {
     public String name;
     public String post;
     public String role;
-    public String depName;
+    public String departName;
 
 
-    public Users(String name, String post, String role, String depName) {
+    public Users(String name, String post, String role, String departName) {
         this.name = name;
         this.post = post;
         this.role = role;
-        this.depName = depName;
+        this.departName = departName;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getDepartName() {
+        return departName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setDepartName(String departName) {
+        this.departName = departName;
+    }
 }
